ef338e65a77eecff6567b1ba13163865fb01ab0e
AriTedeschi <arijun2010@hotmail.com>
2021-06-20 17:04:40 -0300

    feat: API Gateway

--- gateway/src/main/java/com/ari/tedeschi/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/ari/tedeschi/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.ari.tedeschi.departments;
+package com.ari.tedeschi.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class DepartmentsApplication {
+public class GatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DepartmentsApplication.class, args);
+		SpringApplication.run(GatewayApplication.class, args);
 	}
 
 }
